eb7f41c61350ba63c020da8d32ec13767414d06b
FBeron <f.beron10@uniandes.edu.co>
2018-11-09 07:53:17 -0500

    Add files via upload

--- FelipeBeron_Ejercicio26/cuenta.c
+++ FelipeBeron_Ejercicio26/cuenta.c
@@ -8,32 +8,34 @@ int main(int argc, char **argv)
     
     FILE *data, *counts, *time_res;
         
-    char *number;
+    char number;
     int count1 = 0;
     int count2 = 0;
     int count3 = 0;
     int count4 = 0;
     int count5 = 0;
-        
+    
     start = time(0);
         
-    char s1[1] = "1";
-    char s2[2] = "12";
-    char s3[3] = "123";
-    char s4[4] = "1234";
-    char s5[5] = "12345";
+    char c1 = '1';
+    char c2 = '12';
+    char c3[] = '123';
+    char c4[] = '1234';
+    char c5[] = '12345';
     
-    fp = fopen(argv[1],"r");
-    if (!fp)
-            printf("Error abriendo archivo \n");
-    else
-            printf("exito abrir archivo \n");
+    data = fopen(argv[1], "r");
     
-    printf("%d \n", s1[1]);
-    printf("%d \n", s2[2]);
-    printf("%d \n", s2[3]);
-    printf("%d \n", s4[4]);
-    printf("%d \n", s5[5]);
+    while(fscanf(data, "%c", &number) == 1)
+            if (c1 == number)
+            {
+                count1 ++;
+            }
+            
+    
+    //fscanf(data, "%c", &number);
+    //printf("%c\n",number);
+    
+    printf("%i \n", count1);
     
     end = time(0);
     time_taken = (double)(end - start);  
