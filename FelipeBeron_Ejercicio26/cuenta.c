ada16c71aebe54424016b9b437e8296f21f40e8c
FBeron <f.beron10@uniandes.edu.co>
2018-11-09 00:52:18 -0500

    Add files via upload

--- FelipeBeron_Ejercicio26/cuenta.c
+++ FelipeBeron_Ejercicio26/cuenta.c
@@ -1,46 +1,40 @@
 #include <stdio.h> 
 #include <time.h> 
   
-// A function that terminates when enter key is pressed 
-void fun() 
-{ 
-    printf("fun() starts \n"); 
-    printf("Press enter to stop fun \n"); 
-    while(1) 
-    { 
-        if (getchar()) 
-            break; 
-    } 
-    printf("fun() ends \n"); 
-} 
-  
-// The main program calls fun() and measures time taken by fun() 
 int main(int argc, char **argv) 
 {
-    FILE *fp;
+    time_t start, end;
+    double time_taken;
+    
+    FILE *data, *counts, *time_res;
+        
     char *number;
     int count1 = 0;
     int count2 = 0;
     int count3 = 0;
     int count4 = 0;
     int count5 = 0;
-    time_t start, end;
-    double time_taken;
-    
+        
     start = time(0);
-    
-    char sam[5] = "12345";
+        
+    char s1[1] = "1";
+    char s2[2] = "12";
+    char s3[3] = "123";
+    char s4[4] = "1234";
+    char s5[5] = "12345";
     
     fp = fopen(argv[1],"r");
     if (!fp)
             printf("Error abriendo archivo \n");
     else
             printf("exito abrir archivo \n");
-            fscanf(fp,"%s", number);
-            printf("%s\n", number);
     
+    printf("%d \n", s1[1]);
+    printf("%d \n", s2[2]);
+    printf("%d \n", s2[3]);
+    printf("%d \n", s4[4]);
+    printf("%d \n", s5[5]);
     
-    fun(); 
     end = time(0);
     time_taken = (double)(end - start);  
     printf("%f \n",time_taken); 
