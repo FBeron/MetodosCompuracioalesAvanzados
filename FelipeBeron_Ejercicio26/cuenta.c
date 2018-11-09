9354b8b89fca22f03310d510bb05b7d1bb9f5b1e
FBeron <f.beron10@uniandes.edu.co>
2018-11-08 21:20:57 -0500

    Update cuenta.c

--- FelipeBeron_Ejercicio26/cuenta.c
+++ FelipeBeron_Ejercicio26/cuenta.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
+
+FILE * file
 
 int main(int argc, char **argv){
   
+  clock_t t;
+  
+  t = clock();
+  
+  long count_time;
+  char entrada;
+  int count1;
+  int count2;
+  int count3;
+  int count4;
+  int count5;
+  float time;
+  
+  
+  t = clock() - t;
+  double time_taken = ((double)t)/CLOCKS_PER_SEC;
   }
