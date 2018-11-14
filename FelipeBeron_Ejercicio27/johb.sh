ba5bbc2c25adbafbe88e710d7e67a09bf2f88cf1
FBeron <f.beron10@uniandes.edu.co>
2018-11-14 07:24:15 -0500

    Update johb.sh

--- FelipeBeron_Ejercicio27/johb.sh
+++ FelipeBeron_Ejercicio27/johb.sh
@@ -1,7 +1,7 @@
 #PBS -l nodes=4:ppn=1,mem=1gb,walltime=00:10:00
-#PBS -M je.forero@uniandes.edu.co
+#PBS -M f.beron10@uniandes.edu.co
 #PBS -m abe
-#PBS -N ejercicio25
+#PBS -N ejercicio27_Beron
 
 cd $PBS_O_WORKDIR
 make clean
