5b32a4e884a6a333afec9bb8aaa9a5bd57b3d51b
FBeron <f.beron10@uniandes.edu.co>
2018-11-16 07:32:43 -0500

    Update job.sh

--- FelipeBeron_Ejercicio27/job.sh
+++ FelipeBeron_Ejercicio27/job.sh
@@ -1,7 +1,7 @@
 #PBS -l nodes=4:ppn=1,mem=1gb,walltime=00:10:00
 #PBS -M f.beron10@uniandes.edu.co
 #PBS -m abe
-#PBS -N ejercicio27_Beron
+#PBS -N Ejercicio27_Beron
 
 cd $PBS_O_WORKDIR
 rm -f *.dat
